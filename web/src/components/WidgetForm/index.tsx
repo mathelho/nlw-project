59a25eeac129d8e39a01adc374c6b4bf1414f006
mathelho <matheus.coelho789@gmail.com>
2022-05-06 14:42:31 -0300

    Building comment, screenshot and success functionality; finishing front-end

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -6,6 +6,7 @@ import ideaImageUrl from '../../assets/idea.svg'
 import thoughtImageUrl from '../../assets/thought.svg'
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
 import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSuccessStep } from "./Steps/FeedbackSuccessStep";
 
 export const feedbackTypes = {
     BUG: {
@@ -47,20 +48,31 @@ export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
     const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+    const [feedbackSent, setFeedbackSent] = useState(false);
+
+    function handleFeedbackRestart() {
+        setFeedbackSent(false);
+        setFeedbackType(null);
+    }
 
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-            <header>
-                <span className="text-xl leading-6">Deixe seu feedback</span>
 
-                <CloseButton />
-            </header>
-
-            {!feedbackType ? (
-                <FeedbackTypeStep onFeedbackTypeChange={setFeedbackType} />
-            ) : ( 
-                <FeedbackContentStep />
-            )}
+            { feedbackSent ? (
+                <FeedbackSuccessStep onFeedbackRestart={handleFeedbackRestart} />
+            ) : (
+                <>
+                    {!feedbackType ? (
+                        <FeedbackTypeStep onFeedbackTypeChange={setFeedbackType} />
+                    ) : ( 
+                        <FeedbackContentStep 
+                            feedbackTypeChosen={feedbackType} 
+                            onFeedbackRestart={handleFeedbackRestart}
+                            onFeedbackSent={() => setFeedbackSent(true)}
+                        />
+                    )}
+                </>
+            ) }
 
             <footer className="text-xs text-neutral-400">
                 Feito com ♥ pela <a className="underline underline-offset-2" href="https://rocketseat.com.br">Rocketseat</a>
